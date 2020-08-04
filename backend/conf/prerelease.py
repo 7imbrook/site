8069d87b946a7baa17e2825c53acadff50325dc7
Michael Timbrook <timbrook480@gmail.com>
2020-08-03 20:31:12 -0700

    This is templated

--- backend/conf/prerelease.py
+++ backend/conf/prerelease.py
@@ -9,10 +9,6 @@ ALLOWED_HOSTS = [
     "timbrook.dev"
 ]
 
-
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-
 # Import runtime generated configs
 from backend.conf.database import *
 from backend.conf.static import *
