199b877cce9ee3d4db26f182db0c91f81b2aaead
Michael Timbrook <timbrook480@gmail.com>
2020-08-04 21:06:10 -0700

    FIX

--- backend/conf/prerelease.py
+++ backend/conf/prerelease.py
@@ -22,6 +22,8 @@ AWS_S3_ENDPOINT_URL = 'https://sfo2.digitaloceanspaces.com'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
     'loggers': {
         'django': {
             'handlers': ['console'],
