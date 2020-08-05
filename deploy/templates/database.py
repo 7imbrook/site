a844b98b7b1a33ee02ff7b4569df8a31a7bc354c
Michael Timbrook <timbrook480@gmail.com>
2020-08-04 20:43:36 -0700

    Use generated user account for db

--- deploy/templates/database.py
+++ deploy/templates/database.py
@@ -4,14 +4,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'admin',
+        {{ with secret "postgres/creds/site" }}
         'USER': '{{ .Data.username }}',
         'PASSWORD': '{{ .Data.password }}',
+        {{ end }}
         'HOST': '{{ .Data.hostname }}',
         'PORT': '{{ .Data.port }}',
     }
 {{ end }}
 }
-
-{{ with secret "postgres/creds/site" }}
-# {{ .Data }}
-{{ end }}
