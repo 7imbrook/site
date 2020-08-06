b2d8f08170c5d40698bc83dbb3185f10a68865c3
Michael Timbrook <timbrook480@gmail.com>
2020-08-05 21:26:58 -0700

    Public read

--- backend/conf/prerelease.py
+++ backend/conf/prerelease.py
@@ -17,6 +17,8 @@ AWS_LOCATION = 'static'
 AWS_STORAGE_BUCKET_NAME = 'timbrook'
 AWS_S3_REGION_NAME = 'sfo2'
 
+AWS_DEFAULT_ACL = 'public-read'
+
 # Tell django-storages the domain to use to refer to static files.
 AWS_S3_ENDPOINT_URL = 'https://sfo2.digitaloceanspaces.com'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
