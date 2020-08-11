25457ab93bfa609cbef050e1edc73db6f26e955e
Michael Timbrook <timbrook480@gmail.com>
2020-08-10 20:07:47 -0700

    Read new secrets

--- deploy/templates/database.py
+++ deploy/templates/database.py
@@ -1,15 +1,13 @@
 
 DATABASES = {
-{{ with secret "secrets/database/sfo2-default" }}
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'admin',
-        {{ with secret "postgres/creds/site" }}
+        {{ with secret "dbs/creds/site" }}
         'USER': '{{ .Data.username }}',
         'PASSWORD': '{{ .Data.password }}',
         {{ end }}
-        'HOST': '{{ .Data.hostname }}',
-        'PORT': '{{ .Data.port }}',
+        'HOST': '{{ key "site/database/host" }}',
+        'PORT': '{{ key "site/database/port" }}',
     }
-{{ end }}
 }
