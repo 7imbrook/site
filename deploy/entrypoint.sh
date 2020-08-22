043fc5c6dee4926860a7c7e6842adc9d8375e0aa
Michael Timbrook <timbrook480@gmail.com>
2020-08-21 18:41:37 -0700

    This is closerujjdt65y51w1nmqg

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# TODO: this needs set in deploy
+export RELEASE_STAGE=prerelease
+
 echo "Performing pre startup tasks"
 echo "Template bin:" $(which consul-template)
 echo "Consul host:" $CONSUL_HTTP_ADDR
 echo "Vault host:" $VAULT_ADDR
+echo "Stage:" $RELEASE_STAGE
 
-# make migrations
+
+# make migrations, only do this in prerelease
 consul-template -config /opt/deploy/consul-template.hcl -once -exec "python ./manage.py migrate"
+
+# TODO, make sure they we migrate correctly, 
 
 # Run
 consul-template -config /opt/deploy/consul-template.hcl
