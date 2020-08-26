d101d9281751eebfd9739f6d634928b09c84fb14
Michael Timbrook <timbrook480@gmail.com>
2020-08-25 22:09:50 -0700

    Auth and load in token, hacky? Probably

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -12,14 +12,17 @@ echo "Stage:" $RELEASE_STAGE
 cleanup() {
     # Logout of consul here
     echo "Exiting and shutting down"
+     consul logout -token-file ./token
 }
 
 trap cleanup EXIT;
 
+consul login -method kubernetes -token-sink-file ./token -bearer-token-file /var/run/secrets/kubernetes.io/serviceaccount/token
+
 # make migrations, only do this in prerelease
-consul-template -config /opt/deploy/consul-template.hcl -once -exec "python ./manage.py migrate"
+consul-template -config /opt/deploy/consul-template.hcl -once -exec "python ./manage.py migrate" -consul-token=$(cat ./token)
 
 # TODO, make sure they we migrate correctly, 
 
 # Run
-consul-template -config /opt/deploy/consul-template.hcl
+consul-template -config /opt/deploy/consul-template.hcl -consul-token=$(cat ./token)
