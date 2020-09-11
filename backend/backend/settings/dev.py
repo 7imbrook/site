310ba9996109cfd5943b4ca044ffcda792f93315
Michael Timbrook <timbrook480@gmail.com>
2020-09-11 16:23:03 -0700

    Better settings structor

--- backend/backend/settings/dev.py
+++ backend/backend/settings/dev.py
@@ -1,5 +1,7 @@
 import os
 
+from backend.default import *
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -21,3 +23,5 @@ DATABASES = {
     }
 }
 
+TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
+TEST_OUTPUT_DIR = "./test-results"
