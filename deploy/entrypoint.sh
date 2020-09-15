195b3e48945ddda9e28188b8a61ac5aaf3227160
Michael Timbrook <timbrook480@gmail.com>
2020-09-14 19:28:29 -0700

    Broke frontend deploy dep

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -9,20 +9,10 @@ echo "Consul host:" $CONSUL_HTTP_ADDR
 echo "Vault host:" $VAULT_ADDR
 echo "Stage:" $RELEASE_STAGE
 
-cleanup() {
-    # Logout of consul here
-    echo "Exiting and shutting down"
-    consul logout -token-file ./token
-}
-
-trap cleanup EXIT;
-
-consul login -method kubernetes -token-sink-file ./token -bearer-token-file /var/run/secrets/kubernetes.io/serviceaccount/token
-
 # make migrations, only do this in prerelease
-consul-template -config /opt/deploy/consul-template.hcl -once -exec "python ./manage.py migrate" -consul-token=$(cat ./token)
+consul-template -config /opt/deploy/consul-template.hcl -once -exec "python ./manage.py migrate" -consul-token=$(cat /consul/token/consul-token)
 
 # TODO, make sure they we migrate correctly, 
 
 # Run
-consul-template -config /opt/deploy/consul-template.hcl -consul-token=$(cat ./token)
+consul-template -config /opt/deploy/consul-template.hcl -consul-token=$(cat /consul/token/consul-token)
