c73becef2d88ad66e98ca2898a934ba5bd5c44cb
Michael Timbrook <timbrook480@gmail.com>
2020-09-15 00:33:12 -0700

    export env in startup

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -7,12 +7,14 @@ echo "Performing pre startup tasks"
 echo "Template bin:" $(which consul-template)
 echo "Consul host:" $CONSUL_HTTP_ADDR
 echo "Vault host:" $VAULT_ADDR
-echo "Stage:" $RELEASE_STAGE
+echo "DJANGO_SETTINGS_MODULE:" $DJANGO_SETTINGS_MODULE
+
+export CONSUL_HTTP_TOKEN-=$(cat /consul/token/consul-token);
 
 # make migrations, only do this in prerelease
-consul-template -config /opt/deploy/consul-template.hcl -once -exec "python ./manage.py migrate" -consul-token=$(cat /consul/token/consul-token)
+consul-template -config /opt/deploy/consul-template.hcl -once -exec "python ./manage.py migrate"
 
 # TODO, make sure they we migrate correctly, 
 
 # Run
-consul-template -config /opt/deploy/consul-template.hcl -consul-token=$(cat /consul/token/consul-token)
+consul-template -config /opt/deploy/consul-template.hcl 
